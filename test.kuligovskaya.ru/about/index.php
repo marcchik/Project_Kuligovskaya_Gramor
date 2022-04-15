753de6e93af1e3dd8534dcb8bb22258b16e0fa40
marcchik <markstal2002@gmail.com>
2022-04-15 11:45:34 +0300

    feat: add about page

---
+++ test.kuligovskaya.ru/about/index.php
@@ -0,0 +1,44 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("О компании");
+
+?>
+    <main class="main" id="main">
+        <div class="inner-page">
+            <section class="about_company">
+                <div class="wrap">
+                    <div class="title_about_company">О компании</div>
+                    <div class="content_about_company">
+                        <div class="preview_about_company">
+                            <img src="https://test.kuligovskaya.ru/bitrix/templates/test.kuligovskaya.ru/img/main/unsplash_m-jJ4wLjoIc.png" alt="">
+                        </div>
+                        <h2 class="h2_title_about_company">История герба Кулиговских</h2>
+                        <div class="cursive_text_about_company">«Три ноги – в броне, соединенные между собой в центре щита.<br>Ноги расположены в виде треугольника. Сверху – железный шлем».</div>
+                        <p>
+                            Именно такое замысловатое описание имеет родовой герб семьи Кулиговских. Древний род берет свое начало в далеком XI веке. Представители фамилии всегда пользовались почетом и уважением у знати того времени. Право владеть гербом было пожаловано князем Болеславом Кривоустом. Дворянский герб носит название «Драгомир». Подлинная история его возникновения неизвестна. А изображенные на гербе символы расшифровываются историками по-разному. Современные толкования связывают центральное изображение – три ноги в броне, с мобильностью и устойчивостью представителей рода. В смутные средневековые времена Кулиговские могли найти выход из трудных ситуаций. Для укрепления своего материального положения и статуса они использовали торговлю недвижимостью. И сумели достичь в этом деле больших успехов.
+                        </p>
+                        <div class="media_box_about_company">
+                            <a href="./img/main/company1.jpg" class="lazyload company__media__img" data-fslightbox="gallery" data-src="https://test.kuligovskaya.ru/bitrix/templates/test.kuligovskaya.ru/img/main/company1.jpg"></a>
+                            <a href="./video/1.mp4" data-fslightbox="gallery2" data-video-poster="https://fslightbox.com/img/bbunny.png" class="company__media__video" style="background-image: url(https://test.kuligovskaya.ru/bitrix/templates/test.kuligovskaya.ru/img/main/company2.jpg)">
+                                    <span>
+                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                          xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M5 3L19 12L5 21V3Z" fill="white"/>
+                                    </svg>
+
+                                    </span>
+                            </a>
+                            <a href="./img/main/company1.jpg" class="lazyload company__media__img" data-fslightbox="gallery" data-src="https://test.kuligovskaya.ru/bitrix/templates/test.kuligovskaya.ru/img/main/company1.jpg"></a>
+                        </div>
+                        <h2 class="h2_title_about_company">От средневековья до наших дней</h2>
+                        <p>
+                            Архивные источники сообщают, что Кулиговкие произошли от Ивана Кулиговского, который занимал почетную должность судьи в Визской Земле. Предок славился множеством положительных качеств и честно выполнял возложенные на него Законом обязанности. Кулиговский также отличался предприимчивостью в проведении частных сделок с недвижимостью. В 1685 году он удачно продал свою деревню Дроздово. Впоследствии представители рода переселились на более плодородные и выгодные с точки зрения ведения дел земли. Они обосновались, согласно истории, «в деревне Кулиги-Липки, в прежнем Гонифндском Повете лежавшей». Не забывая заслуг Ивана, Кулиговские продолжали выгодно использовать свои умения продавать недвижимость, которой у них, к счастью, было достаточно. То и дело, заключая прибыльные сделки, представители рода Кулиговских нашли в этом свое призвание. И впоследствии стали выступать посредниками между желающими продать или купить деревню, дом, завод, конюшню и т.п. Известность была приобретена во многом благодаря гербу «Драгомир». Он стал хорошо узнаваемой эмблемой, которая и сегодня является символом успешных сделок с недвижимостью.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
