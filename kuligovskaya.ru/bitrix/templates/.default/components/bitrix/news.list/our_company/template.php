5af2405b934bc2a6a2121714732169718dd6de14
marcchik <markstal2002@gmail.com>
2022-04-22 09:42:26 +0300

    feat: add IB our company

---
+++ kuligovskaya.ru/bitrix/templates/.default/components/bitrix/news.list/our_company/template.php
@@ -0,0 +1,76 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+$this->setFrameMode(true);
+?>
+
+<div class="company__wrap">
+    <div class="company__left">
+        <div class="company__numbers">
+            <? foreach($arResult["ITEMS"] as $arItem): ?>
+                <?
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                ?>
+                <? if($arItem['PROPERTIES']['TYPE']['VALUE_XML_ID'] == 1) :?>
+                    <div class="company__numbers__item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                        <div class="big">
+                            <?=$arItem['PROPERTIES']['VALUE_NUMBER']['VALUE']?>
+                            <span> <?=$arItem['PROPERTIES']['PROPS_NUMBER']['VALUE']?></span>
+                        </div>
+                        <div class="small">
+                            <?=$arItem['PREVIEW_TEXT']?>
+                        </div>
+                    </div>
+                <? endif; ?>
+            <? endforeach; ?>
+        </div>
+        <div class="company__media">
+            <? foreach($arResult["ITEMS"] as $arItem): ?>
+                <? if($arItem['PROPERTIES']['TYPE']['VALUE_XML_ID'] == 3) :?>
+                    <a href="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" class="lazyload company__media__img"
+                       data-fslightbox="gallery" data-src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>"></a>
+                    <a href="./video/1.mp4" data-fslightbox="gallery2"
+                       data-video-poster="https://fslightbox.com/img/bbunny.png" class="company__media__video"
+                       style="background-image: url(<?=$arItem['DETAIL_PICTURE']['SRC']?>)">
+                    <span>
+                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 3L19 12L5 21V3Z" fill="white"/>
+                         </svg>
+                    </span>
+                    </a>
+                <? endif; ?>
+            <? endforeach; ?>
+        </div>
+    </div>
+    <div class="company__right">
+        <div class="company__right__wrap">
+            <div class="company__text">
+                <? foreach($arResult["ITEMS"] as $arItem): ?>
+                    <? if($arItem['PROPERTIES']['TYPE']['VALUE_XML_ID'] == 3) :?>
+                        <?=$arItem['PREVIEW_TEXT']?>
+                    <? endif; ?>
+                <? endforeach; ?>
+            </div>
+            <div class="company__list">
+                <? foreach($arResult["ITEMS"] as $arItem): ?>
+                    <? if($arItem['PROPERTIES']['TYPE']['VALUE_XML_ID'] == 2) :?>
+                        <div class="company__list__item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                            <div class="company__list__cross">
+                                <svg width="14.5" height="14.5" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7.25 0V14.5" stroke="white"/>
+                                    <path d="M7.25 0V14.5" stroke="white"/>
+                                    <path d="M7.25 0V14.5" stroke="white"/>
+                                    <path d="M0 7.25L14.5 7.25" stroke="white"/>
+                                    <path d="M0 7.25L14.5 7.25" stroke="white"/>
+                                    <path d="M0 7.25L14.5 7.25" stroke="white"/>
+                                </svg>
+                            </div>
+                            <?=$arItem['PREVIEW_TEXT']?>
+                        </div>
+                    <? endif; ?>
+                <? endforeach; ?>
+            </div>
+            <a href="" class="btn company__btn">Читать дальше</a>
+        </div>
+    </div>
+</div>
+
--- test.kuligovskaya.ru/index.php
+++ test.kuligovskaya.ru/index.php
@@ -247,7 +247,16 @@ $APPLICATION->SetTitle("Главная");
         </div>
         <div class="main-catalog__bot">
             <a href="" class="main-catalog__download">
-                Скачать полный каталог </a>
+                Скачать полный каталог
+                <span>
+                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
+                              stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M7 10L12 15L17 10" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M12 15V3" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </span>
+            </a>
             <div class="wrap">
                 <div class="main-catalog__bot__wrap">
                     <button type="button" class="btn more-catalog-show">Показать еще</button>
@@ -260,62 +269,72 @@ $APPLICATION->SetTitle("Главная");
         <div class="wrap">
             <h2 class="h-title">
                 Наша компания </h2>
-            <div class="company__wrap">
-                <div class="company__left">
-                    <div class="company__numbers">
-                        <div class="company__numbers__item">
-                            <div class="big">
-                                6.5 <span>
-						млн. м<sup> 2</sup> </span>
-                            </div>
-                            <div class="small">
-                                Совокупный объем проектов за 14 лет
-                            </div>
-                        </div>
-                        <div class="company__numbers__item">
-                            <div class="big">
-                                22 Проекта
-                            </div>
-                            <div class="small">
-                                В стадии релализации
-                            </div>
-                        </div>
-                    </div>
-                    <div class="company__media">
-                        <a href="/bitrix/templates/test.kuligovskaya.ru/img/main/company1.jpg"
-                           class="lazyload company__media__img" data-fslightbox="gallery"
-                           data-src="./img/main/company1.jpg"></a> <a
-                                href="/bitrix/templates/test.kuligovskaya.ru/video/1.mp4" data-fslightbox="gallery2"
-                                data-video-poster="https://fslightbox.com/img/bbunny.png" class="company__media__video"
-                                style="background-image: url(/bitrix/templates/test.kuligovskaya.ru/img/main/company2.jpg)"> </a>
-                    </div>
-                </div>
-                <div class="company__right">
-                    <div class="company__right__wrap">
-                        <div class="company__text">
-                            Без комиссий и переплат Закрытые предложения и продажи Все элитные новостройки
-                        </div>
-                        <div class="company__list">
-                            <div class="company__list__item">
-                                <div class="company__list__cross">
-                                </div>
-                                14 работы на рынке недвижимости
-                            </div>
-                            <div class="company__list__item">
-                                <div class="company__list__cross">
-                                </div>
-                                Входим в топ-10 застройщикво по вводу жилья в эксплуатацию
-                            </div>
-                            <div class="company__list__item">
-                                <div class="company__list__cross">
-                                </div>
-                                1500 тыс. м2 введено в эксплуатацию в 2019 году
-                            </div>
-                        </div>
-                        <a href="" class="btn company__btn">Читать дальше</a>
-                    </div>
-                </div>
-            </div>
+            <? $APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"our_company", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "our_company",
+		"DETAIL_URL" => "/catalog/detail?ID=#ELEMENT_ID#",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "DETAIL_PICTURE",
+			1 => "",
+		),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "49",
+		"IBLOCK_TYPE" => "smscity_realty_content",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "SMALL_HEADER",
+			2 => "",
+		),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	),
+	false
+); ?>
             <div class="company__mob">
             </div>
         </div>
